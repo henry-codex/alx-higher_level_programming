6921bd124e5ec6eef57c0bd3c2b050ac801fac09
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-05-09 13:45:49 -0700

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print(f"{number}")
-    else:
-        print(f"{number:02}", end=", ")
-
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 8-uppercase.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-def uppercase(string):
-    """Print a string in uppercase."""
-    for char in string:
-        if ord(char) >= 97 and ord(char) <= 122:
-            char = chr(ord(char) - 32)
-        print(f"{char}", end="")
+def uppercase(str):
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
     print("")
