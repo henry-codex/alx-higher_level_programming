be5f5a4e5db11435e851f0f03d50cbdb1ee90d89
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-05-17 12:05:50 -0700

    addtional updates

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    y = 0
+
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[c]] >= y:
+                res += val[roman_string[c]]
+            else:
+                res -= val[roman_string[c]]
+            y = val[roman_string[c]]
+    return res
