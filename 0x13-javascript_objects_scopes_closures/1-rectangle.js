37c0826a6b481ed78f03240e8ded8ea4422d72ce
henry-codex <httetteh@st.ug.edu.gh>
2023-08-15 10:04:19 +0000

    javascript objects

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Rectangle definition class
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Rectangle double inheritance
+
+const Square1 = require('./5-square.js');
+
+class Square extends Square1 {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
