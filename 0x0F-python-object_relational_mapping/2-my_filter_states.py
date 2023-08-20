8da24d5a23457f2a5060620119f3b2309147439c
henry-codex <httetteh@st.ug.edu.gh>
2023-08-20 01:42:13 -0700

    updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-'''Takes an argument and found a match'''
+''' script for task 2
+    script should take 4 arguments: mysql username,
+    mysql password, database name and state name searched
+'''
+
 import MySQLdb
 import sys
 
 
-def mysqlconnect():
-    db_connection = None
-    db_connection = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+def list_with_name():
+    ''' displays all values in the states table in hbtn db where name
+        matches the argument passed to the script
+    '''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    host = 'localhost'
+    port = 3306
 
-    cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{}'"
-                   "COLLATE Latin1_General_CS;".format(sys.argv[4]))
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute(('SELECT * FROM states WHERE BINARY name = \'{}\'\
+                 ORDER BY id ASC;').format(state_name))
+    result = cur.fetchall()
+    cur.close()
+    db.close()
 
-    states = cursor.fetchall()
+    if result:
+        for row in result:
+            print(row)
 
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db_connection.close()
 
 if __name__ == '__main__':
-    mysqlconnect()
+    list_with_name()
