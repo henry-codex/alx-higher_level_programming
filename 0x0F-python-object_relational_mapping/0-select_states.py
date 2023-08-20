bc9d060f1c560735e21503da516dbd12b10d3993
Tetteh Henry <121228878+henry-codex@users.noreply.github.com>
2023-08-20 07:46:02 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,9 @@
-#!/usr/bin/env python3
-
-'''Connect to the database and list all states'''
-
-import MySQLdb
-import sys
-
-def list_states():
-    db_connection = None
-
-    try:
-        # Connect to the MySQL server
-        db_connection = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-            host='localhost'
-        )
-
-        cursor = db_connection.cursor()
-
-        # Execute the SELECT query to retrieve states
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-
-        # Display the states
-        for state in states:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print("Database Error:", e)
-
-    finally:
-        if db_connection:
-            cursor.close()
-            db_connection.close()
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    list_states()
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+	    id INT NOT NULL AUTO_INCREMENT, 
+	    name VARCHAR(256) NOT NULL,
+	    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
